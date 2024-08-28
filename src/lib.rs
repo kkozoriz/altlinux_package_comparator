c12f2c269665a78b3f92e3a3a466a63ad11973a1
kkozoriz <kkozoriz7@yandex.ru>
2024-08-28 19:58:53 +0300

    feat: Added the main logic of the app
    Added a function to get the response from the API, a json response structure, functions to calculate package and version differences

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,73 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use log::info;
+use reqwest::{Error, Response};
+use serde::{Deserialize, Serialize};
+use std::collections::{hash_set::Difference, HashSet};
+use std::hash::{Hash, Hasher, RandomState};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Package {
+    name: String,
+    version: String,
+    release: String,
+    arch: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+// TODO: Put it in a separate module
+impl PartialEq for Package {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
     }
+}
+
+impl Eq for Package {}
+
+impl Hash for Package {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct ApiResponse {
+    packages: HashSet<Package>,
+}
+
+// TODO: Put it in a separate module
+async fn fetch_packages(branch: &str, arch: &str, url: &str) -> Result<HashSet<Package>, Error> {
+    let response: Response = match arch.is_empty() {
+        true => reqwest::get(format!("{}/{}", url, branch)).await?,
+        false => reqwest::get(format!("{}/{}?arch={}", url, branch, arch)).await?,
+    };
+    let api_response: ApiResponse = response.json().await?;
+
+    Ok(api_response.packages)
+}
+
+// TODO: Add the ability to output to console or file and await on operations
+pub async fn process_branch_packages(
+    first_branch: &str,
+    second_branch: &str,
+    arch: &str,
+    url: &str,
+) -> Result<(), Error> {
+    let first_packages_set: HashSet<Package> = fetch_packages(first_branch, arch, url).await?;
+    let second_packages_set: HashSet<Package> = fetch_packages(second_branch, arch, url).await?;
+
+    info!("Start processing...");
+    let _diff_1: Difference<Package, RandomState> =
+        first_packages_set.difference(&second_packages_set);
+    let _diff_2: Difference<Package, RandomState> =
+        second_packages_set.difference(&first_packages_set);
+
+    let mut version_le_vec: Vec<&Package> = vec![];
+
+    for pkg1 in &first_packages_set {
+        if let Some(pkg2) = second_packages_set.get(pkg1) {
+            if pkg1.version > pkg2.version {
+                version_le_vec.push(pkg1)
+            }
+        }
+    }
+
+    Ok(())
 }
