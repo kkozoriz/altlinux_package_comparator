66a65543530783bb6e4fa8c26ea2e75b32d11e99
kkozoriz <kkozoriz7@yandex.ru>
2024-08-29 17:18:10 +0300

    feat: added commands, options and arguments to cli
    Added various commands, as well as the ability to specify a result file for outputting the result, a small refactoring was done

--- src/cli.rs
+++ src/cli.rs
@@ -1,20 +1,39 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
-#[derive(Parser, Debug)]
-#[command(name = "pkg-cmp")]
+#[derive(Parser)]
+#[command(bin_name = "pkg-cmp")]
 #[command(version = "0.1.0")]
 #[command(about = "Command line arguments for alt linux package comparator", long_about = None)]
 pub struct Args {
+    #[command(subcommand)]
+    command: CliCommands,
+
+    /// Path to the output file
+    #[arg(short = 'o', long = "output-file")]
+    output_file: Option<PathBuf>,
+
+    /// Architecture to filter the packages (optional)
     #[arg(short = 'a', long = "arch")]
     arch: Option<String>,
 
-    /// name of first comparable branch, by default - sisyphus
-    #[arg(short = 'f', long = "first-branch", default_value = "sisyphus")]
+    /// Name of first comparable branch
+    #[arg(default_value = "sisyphus")]
     first_branch: String,
 
-    /// name of second comparable branch, by default - p10
-    #[arg(short = 's', long = "second_branch", default_value = "p10")]
+    /// Name of second comparable branch
+    #[arg(default_value = "p10")]
     second_branch: String,
+}
+
+#[derive(Subcommand)]
+pub enum CliCommands {
+    /// Find packages that are only in the first branch
+    FirstBranchOnly,
+    /// Find packages that are only in the second branch
+    SecondBranchOnly,
+    /// Find packages where the version-release is newer in the sisyphus than in the p10
+    SisyphusNewer,
 }
 
 impl Args {
@@ -26,5 +45,11 @@ impl Args {
     }
     pub fn second_branch(&self) -> &str {
         self.second_branch.as_str()
+    }
+    pub fn command(&self) -> &CliCommands {
+        &self.command
+    }
+    pub fn output_file(&self) -> &Option<PathBuf> {
+        &self.output_file
     }
 }
